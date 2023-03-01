ca7a886b4aa30005bee4afcc4b3bb66382067a38
Gergely Göndör <xgondor@stuba.sk>
2023-03-01 13:06:44 +0100

    Zadanie1_Final_Ready
    Translate all to english
    Clear up unnecessary code
    Simplify algorithm
    Waitkey for imgshow mosaiks

--- camera_test.py
+++ camera_test.py
@@ -1,5 +1,10 @@
-
+from ximea import xiapi
 import cv2
+import numpy as np
+from PIL import Image
+import os
+from PIL.ExifTags import TAGS
+
 ### runn this command first echo 0|sudo tee /sys/module/usbcore/parameters/usbfs_memory_mb  ###
 
 #create instance for first connected camera
@@ -27,43 +32,34 @@ img = xiapi.Image()
 print('Starting data acquisition...')
 cam.start_acquisition()
 
-#variables
-k=1
+img_counter = 0
 
-while cv2.waitKey() != ord('q') :
-    cam.get_image(img)
-    image = img.get_image_data_numpy()
-    image = cv2.resize(image,(240,240))
-    cv2.imshow("test", image)
+while True:
+    # Initializing the frame, ret
+    ret, frame = cam.read()
 
-    if k < 5 :
-        cv2.imwrite(str(k) +".jpg", image)
-        k = k+1
+    if not ret:
+        print('failed to grab frame')
+        break
 
-    cv2.waitKey()
+    cv.imshow('Webcam image', frame)
+    # To get continuous live video feed from my laptops webcam
+    k = cv.waitKey(1)
 
-print('test')
+    if k%256 == 27: #ESC pressed - close app
+        print('ESC pressed, closing the app')
+        exit()
+    elif k%256  == 32: # SPACE pressed - take picture
+        img_name = "img{}.jpg".format(img_counter+1)
+        frame = cv.resize(frame, (240, 240))
+        cv.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
 
-# for i in range(10):
-#     #get data and pass them from camera to img
-#     cam.get_image(img)
-#     image = img.get_image_data_numpy()
-#     cv2.imshow("test", image)
-#     cv2.waitKey()
-#     #get raw data from camera
-#     #for Python2.x function returns string
-#     #for Python3.x function returns bytes
-#     data_raw = img.get_image_data_raw()
-#
-#     #transform data to list
-#     data = list(data_raw)
-#
-#     #print image data and metadata
-#     print('Image number: ' + str(i))
-#     print('Image width (pixels):  ' + str(img.width))
-#     print('Image height (pixels): ' + str(img.height))
-#     print('First 10 pixels: ' + str(data[:10]))
-#     print('\n')
+    if img_counter == 4:
+        print("{} images taken!".format(img_counter))
+        break
+
 
 #stop data acquisition
 print('Stopping acquisition...')
@@ -72,4 +68,57 @@ cam.stop_acquisition()
 #stop communication
 cam.close_device()
 
-print('Done.')
+
+# Read images
+img1 = cv.imread('img1.jpg')
+img2 = cv.imread('img2.jpg')
+img3 = cv.imread('img3.jpg')
+img4 = cv.imread('img4.jpg')
+
+# Create mosaik
+
+vis1 = np.concatenate((img1, img2), axis=1)
+vis2 = np.concatenate((img3, img4), axis=1)
+
+vis = np.concatenate((vis1, vis2), axis=0)
+
+cv.imwrite('mosaik.jpg', vis)
+
+cv.imshow('Mosaik', vis)
+
+cv.waitKey()
+
+# 1. image: Kernel mask
+
+kernel = np.array([[0, -1, 0],
+                   [-1, 5, -1],
+                   [0, -1, 0]])
+
+vis[0:240, 0:240] = cv.filter2D(vis[0:240, 0:240], -1, kernel)
+cv.imwrite('mosaik_final.jpg', vis)
+
+# 2. image: Rotate by 90 degrees
+
+for j in range (0,240):
+    for k in range (0,240):
+        vis[j, 240 + k] = img2[239-k, j]
+
+cv.imwrite('mosaik_final.jpg', vis)
+
+# 3. image: channel R
+
+vis[240:480, 0:240, 0] = 0
+vis[240:480, 0:240, 1] = 0
+
+cv.imwrite('mosaik_final.jpg', vis)
+
+# Information about mosaic
+
+image = Image.open('mosaik_final.jpg')
+cv.imshow('Final mosaik', vis)
+
+print('\nData type: \t'+str(image.format))
+print('Image size: '+str(image.size))
+print('File size:\t'+str(os.path.getsize('mosaik_final.jpg'))+' bytes')
+
+cv.waitKey()
