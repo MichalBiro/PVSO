b7430cb9fabc6aaad0196cec858030a2198b5bee
Gergely Göndör <xgondor@stuba.sk>
2023-03-14 20:48:01 +0100

    Update main.py

--- Zadanie_2/main.py
+++ Zadanie_2/main.py
@@ -15,61 +15,61 @@ cam = xiapi.Camera()
 #to open specific device, use:
 #cam.open_device_by_SN('41305651')
 #(open by serial number)
-# print('Opening first camera...')
-# cam.open_device()
-#
-# #settings
-# cam.set_exposure(10000)
-# cam.set_param('imgdataformat','XI_RGB32')
-# cam.set_param('auto_wb', 1)
-# print('Exposure was set to %i us' %cam.get_exposure())
-#
-# #create instance of Image to store image data and metadata
-# img = xiapi.Image()
-#
-# #start data acquisition
-# print('Starting data acquisition...')
-# cam.start_acquisition()
-#
-# img_counter = 0
-#
-# wanted_image_count = 10
-#
-# while True:
-#     # Initializing the frame, ret
-#     cam.get_image(img)
-#     frame = img.get_image_data_numpy()
-#     frame = cv.resize(frame, (800, 800))
-#
-#     # if not ret:
-#     #    print('failed to grab frame')
-#     #    break
-#
-#     cv.imshow('Webcam image', frame)
-#     # To get continuous live video feed from my laptops webcam
-#     k = cv.waitKey(1)
-#
-#     if k%256 == 27: # ESC pressed - close app
-#         print('ESC pressed, closing the app')
-#         exit()
-#     elif k%256  == 32: # SPACE pressed - take picture
-#         img_name = "img{}.jpg".format(img_counter+1)
-#         frame = cv.resize(frame, (800, 800))
-#         cv.imwrite(img_name, frame)
-#         print("{} written!".format(img_name))
-#         img_counter += 1
-#
-#     if img_counter == wanted_image_count:
-#         print("{} images taken!".format(img_counter))
-#         break
-#
-#
-# #stop data acquisition
-# print('Stopping acquisition...')
-# cam.stop_acquisition()
-#
-# #stop communication
-# cam.close_device()
+print('Opening first camera...')
+cam.open_device()
+
+#settings
+cam.set_exposure(10000)
+cam.set_param('imgdataformat','XI_RGB32')
+cam.set_param('auto_wb', 1)
+print('Exposure was set to %i us' %cam.get_exposure())
+
+#create instance of Image to store image data and metadata
+img = xiapi.Image()
+
+#start data acquisition
+print('Starting data acquisition...')
+cam.start_acquisition()
+
+img_counter = 0
+
+wanted_image_count = 10
+
+while True:
+    # Initializing the frame, ret
+    cam.get_image(img)
+    frame = img.get_image_data_numpy()
+    frame = cv.resize(frame, (800, 800))
+
+    # if not ret:
+    #    print('failed to grab frame')
+    #    break
+
+    cv.imshow('Webcam image', frame)
+    # To get continuous live video feed from my laptops webcam
+    k = cv.waitKey(1)
+
+    if k%256 == 27: # ESC pressed - close app
+        print('ESC pressed, closing the app')
+        exit()
+    elif k%256  == 32: # SPACE pressed - take picture
+        img_name = "img{}.jpg".format(img_counter+1)
+        frame = cv.resize(frame, (800, 800))
+        cv.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+    if img_counter == wanted_image_count:
+        print("{} images taken!".format(img_counter))
+        break
+
+
+#stop data acquisition
+print('Stopping acquisition...')
+cam.stop_acquisition()
+
+#stop communication
+cam.close_device()
 
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -87,7 +87,7 @@ for fname in images:
     ret, corners = cv.findChessboardCorners(gray, (7,5), None)
     # If found, add object points, image points (after refining them)
     if ret:
-        print("test")
+        print("ret")
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
@@ -123,5 +123,48 @@ dst = cv.undistort(img, mtx, dist, None, newcameramtx)
 x, y, w, h = roi
 dst = dst[y:y+h, x:x+w]
 cv.imwrite('calibresult.png', dst)
+
+
+for fname in images:
+    img = cv.imread(fname)
+    if not ret:
+        print('failed to grab frame')
+        break
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    gray = cv.medianBlur(gray, 5)
+
+    rows = gray.shape[0]
+    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
+                              param1=200, param2=30,
+                              minRadius=0, maxRadius=0)
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv.circle(frame, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv.circle(frame, center, radius, (255, 0, 255), 3)
+
+    cv.imshow("detected circles", frame)
+
+    # cv.imshow('Webcam image', frame)
+    # To get continuous live video feed from my laptops webcam
+    k = cv.waitKey(1)
+
+    if k % 256 == 27:  # ESC pressed - close app
+        print('ESC pressed, closing the app')
+        exit()
+    elif k % 256 == 32:  # SPACE pressed - take picture
+        img_name = "img.jpg"
+        # frame = cv.resize(frame, (240, 240))
+        # cv.imwrite(img_name, frame)
+        # print("{} written!".format(img_name))
+        # src = cv.imread('img.jpg')
+
 
 print('end')
